3875f342a6c8760db483da4056843582667d6a47
DenizAlakavuklar <deniz.kovanci@gmail.com>
2023-02-07 15:40:26 +0100

    Still working on the iteration 2 - have something working :)

--- models/User.model.js
+++ models/User.model.js
@@ -1,21 +1,36 @@
 const { Schema, model } = require("mongoose");
 
+const validateEmail = function (email) {
+  var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+  return re.test(email)
+}
 // TODO: Please make sure you edit the user model to whatever makes sense in this case
 const userSchema = new Schema({
   username: {
     type: String,
-    
-    unique: true
+    unique: true,
+    required: false,
+      unique: true,
   },
   email: {
     type: String,
-    required: true
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true,
+    validate: [validateEmail, 'Please fill a valid email address'],
+    match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address'],
   },
   passwordHash: {
     type: String,
-    required: true
-  }
-});
+    required: true,
+  },
+},
+{
+  // this second object adds extra properties: `createdAt` and `updatedAt`
+  timestamps: true,
+}
+);
 
 const User = model("User", userSchema);
 
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,34 +1,71 @@
-const router = require("express").Router()
+const express = require('express')
+const bcrypt = require('bcryptjs')
+const User = require('../models/User.model')
+const router = express.Router()
 
-const User = require("../models/User.model")
+/* GET home page */
+router.get('/signup', (req, res) => {
+  res.render('auth/signup')
+})
 
-const bcrypt = require("bcryptjs")
+router.post('/signup', async (req, res) => {
+  const body = { ...req.body }
 
-
-
-/* GET auth page */
-router.get("/signup", (req, res) => {
-  res.render("signup");
-});
-
-router.post("/signup", async(req,res) => {
-    const body = {...req.body}
-    console.log(body)
+  if (body.password.length < 6) {
+    res.render('auth/signup', { errorMessage: 'Password too short', body: req.body })
+  } else {
     const salt = bcrypt.genSaltSync(13)
     const passwordHash = bcrypt.hashSync(body.password, salt)
-    console.log (passwordHash)
+    console.log(passwordHash)
 
     delete body.password
     body.passwordHash = passwordHash
 
-    try{
-        console.log ("working?")
-     await User.create(body)
-     res.send (body) 
+    try {
+      await User.create(body)
+     // res.send(body)
+     res.render("profile")
+    } catch (error) {
+      if (error.code === 11000) {
+        console.log('Duplicate !')
+        res.render('auth/signup', {
+          errorMessage: 'Username already used !',
+          userData: req.body,
+        })
+      } else {
+        res.render('auth/signup', {
+          errorMessage: error,
+          userData: req.body,
+        })
+      }
     }
-    catch (error){console.log(error)
-
-    } 
+  }
 })
 
-module.exports = router;
+router.get('/login', (req, res) => {
+  res.render('auth/login')
+})
+
+router.post('/login', async (req, res) => {
+  console.log('SESSION =====> ', req.session)
+  const body = req.body
+
+  const userMatch = await User.find({ username: body.username })
+  console.log(userMatch)
+  if (userMatch.length) {
+    // User found
+    const user = userMatch[0]
+
+    if (bcrypt.compareSync(body.password, user.passwordHash)) {
+      // Correct password
+      console.log(user)
+      res.render('profile', { user })
+    } else {
+      // Incorrect password
+    }
+  } else {
+    // User not found
+  }
+})
+
+module.exports = router
