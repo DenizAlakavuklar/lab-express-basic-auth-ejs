d9800e805f95c39e9c569c79f1c0019e65a0ab23
DenizAlakavuklar <deniz.kovanci@gmail.com>
2023-02-07 20:08:59 +0100

    Iteration 3 - /main is completed.

--- routes/index.js
+++ routes/index.js
@@ -8,10 +8,13 @@ router.get("/", (req, res,) => {
   res.render("index");
 });
 
+router.get("/main", (req, res,) => {
+  res.render("main");
+});
+
 router.get('/profile', isLoggedIn, (req, res) => {
   res.render('profile',{ user: req.session.user })
 })
-
 
 
 
